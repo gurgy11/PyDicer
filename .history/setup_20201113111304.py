21b82ab0d35021229ffe9c4ea52d42317d031d59
Gregory Bockus <gregory.bockus@gmail.com>
2020-11-13 12:00:29 -0500

    commit on main virtual environment

---
+++ .history/setup_20201113111304.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dicepy',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+<<<<<<< HEAD
+        'flask', 
+        'marshmallow',
+        'mysql-connector-python',
+        'openpyxl',
+        'pandas',
+        'python-dotenv'
+    ]
+)
+=======
+        'flask',
+        'marshmallow',
+        'mysql-connector-python',
+        'openpyxl',
+        'pandas'
+    ]
+)
+>>>>>>> c55cf40df8a4bd0c980ecf72dfa8d6c8726aafd2
